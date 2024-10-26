cf904fa14cb17fe89e30895b152a6c6a1d98c532
azhaannazim <132593214+azhaannazim@users.noreply.github.com>
2024-10-26 14:06:10 +0530

    B- > b

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -4,7 +4,7 @@ import Modal from "./Modal";
 import Authentication from "./Authentication";
 import { useAuth } from "../context/AuthContext";
 import { doc, setDoc } from "firebase/firestore";
-import { db } from "../../fireBase";
+import { db } from "../../firebase";
 
 export default function CoffeeForm(props){
     const {isAuthenticated} = props
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth, db } from "../../fireBase";
+import { auth, db } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = createContext()
